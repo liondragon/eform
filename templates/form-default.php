f9c2e7c0bf56fabaa09b38a6adf67c3a6859f869
James Alexander <liondragon88@gmail.com>
2025-08-09 16:27:56 -0600

    fix regex in phone field pattern

--- templates/form-default.php
+++ templates/form-default.php
@@ -17,7 +17,7 @@
         <div class="columns_nomargins inputwrap">
             <?php eform_field('phone', [
                 'required' => true,
-                'pattern'  => '\\s*(?:\\(\\d{3}\\)|\\d{3})[-.\\s]?\\d{3}[-.\\s]?\\d{4}\\s*',
+                'pattern'  => '(?:\\(\\d{3}\\)|\\d{3})[ .-]?\\d{3}[ .-]?\\d{4}',
                 'title'    => 'U.S. phone number (10 digits)',
             ]); ?>
             <?php eform_field_error('phone'); ?>
