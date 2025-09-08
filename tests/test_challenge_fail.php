9f5f32e55f44f011bafc7c6b1e9699cbd6c59f10
James Alexander <liondragon88@gmail.com>
2025-09-08 17:09:41 -0600

    feat: test challenge soft-fail handling

---
+++ tests/test_challenge_fail.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+putenv('EFORMS_COOKIE_MISSING_POLICY=challenge');
+putenv('EFORMS_CHALLENGE_MODE=auto');
+putenv('EFORMS_CHALLENGE_PROVIDER=turnstile');
+putenv('EFORMS_TURNSTILE_SITE_KEY=site');
+putenv('EFORMS_TURNSTILE_SECRET_KEY=secret');
+putenv('EFORMS_LOG_LEVEL=1');
+require __DIR__ . '/bootstrap.php';
+$_SERVER['REQUEST_METHOD'] = 'POST';
+$_SERVER['HTTP_REFERER'] = 'http://hub.local/form-test/';
+$_POST = [
+    'form_id' => 'contact_us',
+    'instance_id' => 'instCHFAIL',
+    'timestamp' => time() - 5,
+    'eforms_hp' => '',
+    'js_ok' => '1',
+    'name' => 'Zed',
+    'email' => 'zed@example.com',
+    'message' => 'Ping',
+    'cf-turnstile-response' => 'fail',
+];
+$fm = new \EForms\FormManager();
+$fm->handleSubmit();
