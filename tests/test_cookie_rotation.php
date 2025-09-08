4146aa42ee69645406c18c579d0b4c4d13972429
James Alexander <liondragon88@gmail.com>
2025-09-08 15:25:22 -0600

    Add tests for token modes and honeypot

---
+++ tests/test_cookie_rotation.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+require __DIR__ . '/bootstrap.php';
+
+$_SERVER['REQUEST_METHOD'] = 'POST';
+$_SERVER['HTTP_REFERER'] = 'http://hub.local/form-test/';
+$_COOKIE['eforms_t_contact_us'] = 'oldCookie';
+$_POST = [
+    'form_id' => 'contact_us',
+    'instance_id' => 'instROT',
+    'timestamp' => time(),
+    'eforms_hp' => '',
+    'name' => 'Zed',
+    'email' => 'zed@example.com',
+    'message' => 'Ping',
+];
+
+register_shutdown_function(function () {
+    file_put_contents(__DIR__ . '/tmp/cookie.txt', $_COOKIE['eforms_t_contact_us'] ?? '');
+});
+
+$fm = new \EForms\FormManager();
+ob_start();
+$fm->handleSubmit();
+ob_end_clean();
---
+++ tests/test_honeypot_capture.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+putenv('EFORMS_LOG_LEVEL=1');
+require __DIR__ . '/bootstrap.php';
+
+$_SERVER['REQUEST_METHOD'] = 'POST';
+$_SERVER['HTTP_REFERER'] = 'http://hub.local/form-test/';
+$_COOKIE['eforms_t_contact_us'] = 'tokHP';
+$_POST = [
+    'form_id' => 'contact_us',
+    'instance_id' => 'instHP',
+    'timestamp' => time(),
+    'eforms_hp' => 'bot',
+    'name' => '',
+    'email' => '',
+    'message' => '',
+];
+
+$fm = new \EForms\FormManager();
+ob_start();
+$fm->handleSubmit();
+ob_end_clean();
