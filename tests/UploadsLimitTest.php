04a78b15cc8477abf35d9a1ccb2e113f18b31bb4
James Alexander <liondragon88@gmail.com>
2025-09-11 12:42:49 -0600

    Add tests for email attachment overflow and success cookie

--- tests/UploadsLimitTest.php
+++ tests/UploadsLimitTest.php
@@ -3,9 +3,28 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 use EForms\Uploads;
+use EForms\Config;
 
 final class UploadsLimitTest extends TestCase
 {
+    private function rebootstrap(array $overrides): void
+    {
+        global $TEST_FILTERS;
+        $TEST_FILTERS = [];
+        $ref = new \ReflectionClass(Config::class);
+        $boot = $ref->getProperty('bootstrapped');
+        $boot->setAccessible(true);
+        $boot->setValue(false);
+        $data = $ref->getProperty('data');
+        $data->setAccessible(true);
+        $data->setValue([]);
+        add_filter('eforms_config', function ($defaults) use ($overrides) {
+            $defaults['uploads'] = array_replace($defaults['uploads'], $overrides);
+            return $defaults;
+        });
+        Config::bootstrap();
+    }
+
     public function testMaxFileBytesPerField(): void
     {
         $tpl = [
@@ -53,6 +72,70 @@ final class UploadsLimitTest extends TestCase
         $res = Uploads::normalizeAndValidate($tpl, $files);
         $this->assertArrayHasKey('docs', $res['errors']);
         $this->assertSame('Too many files.', $res['errors']['docs'][0]);
+        @unlink($tmp1);
+        @unlink($tmp2);
+    }
+
+    public function testMaxFieldTotalBytes(): void
+    {
+        $this->rebootstrap(['total_field_bytes' => 100]);
+        $tpl = [
+            'fields' => [
+                ['type' => 'files', 'key' => 'docs', 'accept' => ['pdf']],
+            ],
+        ];
+        $tmp1 = tempnam(sys_get_temp_dir(), 'up');
+        file_put_contents($tmp1, "%PDF-1.4\n" . str_repeat('A', 60));
+        $tmp2 = tempnam(sys_get_temp_dir(), 'up');
+        file_put_contents($tmp2, "%PDF-1.4\n" . str_repeat('A', 60));
+        $files = [
+            'docs' => [
+                'name' => ['a.pdf', 'b.pdf'],
+                'type' => ['application/pdf', 'application/pdf'],
+                'tmp_name' => [$tmp1, $tmp2],
+                'error' => [UPLOAD_ERR_OK, UPLOAD_ERR_OK],
+                'size' => [filesize($tmp1), filesize($tmp2)],
+            ],
+        ];
+        $res = Uploads::normalizeAndValidate($tpl, $files);
+        $this->assertArrayHasKey('docs', $res['errors']);
+        $this->assertSame('This file exceeds the size limit.', $res['errors']['docs'][0]);
+        @unlink($tmp1);
+        @unlink($tmp2);
+    }
+
+    public function testMaxTotalRequestBytes(): void
+    {
+        $this->rebootstrap(['total_request_bytes' => 100]);
+        $tpl = [
+            'fields' => [
+                ['type' => 'file', 'key' => 'a', 'accept' => ['pdf']],
+                ['type' => 'file', 'key' => 'b', 'accept' => ['pdf']],
+            ],
+        ];
+        $tmp1 = tempnam(sys_get_temp_dir(), 'up');
+        file_put_contents($tmp1, "%PDF-1.4\n" . str_repeat('A', 60));
+        $tmp2 = tempnam(sys_get_temp_dir(), 'up');
+        file_put_contents($tmp2, "%PDF-1.4\n" . str_repeat('A', 60));
+        $files = [
+            'a' => [
+                'name' => 'a.pdf',
+                'type' => 'application/pdf',
+                'tmp_name' => $tmp1,
+                'error' => UPLOAD_ERR_OK,
+                'size' => filesize($tmp1),
+            ],
+            'b' => [
+                'name' => 'b.pdf',
+                'type' => 'application/pdf',
+                'tmp_name' => $tmp2,
+                'error' => UPLOAD_ERR_OK,
+                'size' => filesize($tmp2),
+            ],
+        ];
+        $res = Uploads::normalizeAndValidate($tpl, $files);
+        $this->assertArrayHasKey('_global', $res['errors']);
+        $this->assertSame('File upload failed. Please try again.', $res['errors']['_global'][0]);
         @unlink($tmp1);
         @unlink($tmp2);
     }
