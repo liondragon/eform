e481d76c84659c1acf2dda6bed5c55402123b5e5
James Alexander <liondragon88@gmail.com>
2025-09-13 12:44:57 -0600

    Reset $_GET in tests and restore snapshots

--- tests/integration/test_success_inline.php
+++ tests/integration/test_success_inline.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 // Simulate that a success cookie is present and renderer should show success message
+$getSnapshot = $_GET;
+$cookieSnapshot = $_COOKIE;
 $_GET['eforms_success'] = 'contact_us';
 $_COOKIE['eforms_s_contact_us'] = 'contact_us:instOK';
 
@@ -9,4 +11,7 @@ require __DIR__ . '/../bootstrap.php';
 $fm = new \EForms\Rendering\FormManager();
 $html = $fm->render('contact_us');
 file_put_contents(__DIR__ . '/../tmp/out_success_inline.html', $html);
+
+$_GET = $getSnapshot;
+$_COOKIE = $cookieSnapshot;
 
--- tests/unit/SuccessAntiSpoofTest.php
+++ tests/unit/SuccessAntiSpoofTest.php
@@ -15,54 +15,79 @@ final class SuccessAntiSpoofTest extends BaseTestCase
     public function testNoSuccessWithQueryOnly(): void
     {
         header_remove();
-        $_GET = ['eforms_success' => 'contact_us'];
-        $_COOKIE = [];
-        $fm = new \EForms\Rendering\FormManager();
-        $html = $fm->render('contact_us');
-        $this->assertSame('', $html);
+        $getSnapshot = $_GET;
+        try {
+            $_GET = ['eforms_success' => 'contact_us'];
+            $_COOKIE = [];
+            $fm = new \EForms\Rendering\FormManager();
+            $html = $fm->render('contact_us');
+            $this->assertSame('', $html);
+        } finally {
+            $_GET = $getSnapshot;
+        }
     }
 
     public function testNoSuccessWithCookieOnly(): void
     {
         header_remove();
-        $_GET = [];
-        $_COOKIE = ['eforms_s_contact_us' => 'contact_us:inst'];
-        $fm = new \EForms\Rendering\FormManager();
-        $html = $fm->render('contact_us');
-        $this->assertStringNotContainsString('Thanks! We got your message.', $html);
+        $getSnapshot = $_GET;
+        try {
+            $_GET = [];
+            $_COOKIE = ['eforms_s_contact_us' => 'contact_us:inst'];
+            $fm = new \EForms\Rendering\FormManager();
+            $html = $fm->render('contact_us');
+            $this->assertStringNotContainsString('Thanks! We got your message.', $html);
+        } finally {
+            $_GET = $getSnapshot;
+        }
     }
 
     public function testNoSuccessWithMismatchedCookie(): void
     {
         header_remove();
-        $_GET = ['eforms_success' => 'contact_us'];
-        $_COOKIE = ['eforms_s_contact_us' => 'other:inst'];
-        $fm = new \EForms\Rendering\FormManager();
-        $html = $fm->render('contact_us');
-        $this->assertSame('', $html);
+        $getSnapshot = $_GET;
+        try {
+            $_GET = ['eforms_success' => 'contact_us'];
+            $_COOKIE = ['eforms_s_contact_us' => 'other:inst'];
+            $fm = new \EForms\Rendering\FormManager();
+            $html = $fm->render('contact_us');
+            $this->assertSame('', $html);
+        } finally {
+            $_GET = $getSnapshot;
+        }
     }
 
     public function testSuccessWithMatchingCookieAndQuery(): void
     {
         header_remove();
-        $_GET = ['eforms_success' => 'contact_us'];
-        $_COOKIE = ['eforms_s_contact_us' => 'contact_us:inst'];
-        $fm = new \EForms\Rendering\FormManager();
-        $html = $fm->render('contact_us');
-        $this->assertStringContainsString('Thanks! We got your message.', $html);
+        $getSnapshot = $_GET;
+        try {
+            $_GET = ['eforms_success' => 'contact_us'];
+            $_COOKIE = ['eforms_s_contact_us' => 'contact_us:inst'];
+            $fm = new \EForms\Rendering\FormManager();
+            $html = $fm->render('contact_us');
+            $this->assertStringContainsString('Thanks! We got your message.', $html);
+        } finally {
+            $_GET = $getSnapshot;
+        }
     }
 
     public function testSuccessCookieConsumedOnlyOnce(): void
     {
         header_remove();
-        $_GET = ['eforms_success' => 'contact_us'];
-        $_COOKIE = ['eforms_s_contact_us' => 'contact_us:inst'];
-        $fm = new \EForms\Rendering\FormManager();
-        $html1 = $fm->render('contact_us');
-        $this->assertStringContainsString('Thanks! We got your message.', $html1);
-        $fm2 = new \EForms\Rendering\FormManager();
-        $html2 = $fm2->render('contact_us');
-        $this->assertSame('', $html2);
+        $getSnapshot = $_GET;
+        try {
+            $_GET = ['eforms_success' => 'contact_us'];
+            $_COOKIE = ['eforms_s_contact_us' => 'contact_us:inst'];
+            $fm = new \EForms\Rendering\FormManager();
+            $html1 = $fm->render('contact_us');
+            $this->assertStringContainsString('Thanks! We got your message.', $html1);
+            $fm2 = new \EForms\Rendering\FormManager();
+            $html2 = $fm2->render('contact_us');
+            $this->assertSame('', $html2);
+        } finally {
+            $_GET = $getSnapshot;
+        }
     }
 }
 
