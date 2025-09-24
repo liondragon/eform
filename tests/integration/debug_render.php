e5c3ff09459f10518b9ad2c10df6544c0b9aee96
James Alexander <liondragon88@gmail.com>
2025-09-24 16:17:44 -0600

    Delete tests directory

--- tests/integration/debug_render.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-require __DIR__ . '/../bootstrap.php';
-$renderer = new EForms\Rendering\FormRenderer();
-echo $renderer->render($argv[1] ?? 'contact_us', []);
-
--- tests/integration/test_render_nocache.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-require __DIR__ . '/../bootstrap.php';
-
-$renderer = new \EForms\Rendering\FormRenderer();
-echo $renderer->render('contact_us', ['cacheable' => false]);
